93fb2b6d9d0bf258ab379a5e72650c940d9d90f1
Hiratsuna <adhdmicromanagement@outlook.com>
2023-11-05 18:51:52 +0200

    add RSS logic, what is left is to figure out the message send property to the correct channel  and see what else is there to modify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
+const fs = require("fs");
+const Parser = require("rss-parser");
 
 const client = new Client({
     intents: [
@@ -10,61 +12,69 @@ const client = new Client({
     ]
 })
 
+(async function main() {
+
+    // Make a new RSS Parser
+    const parser = new Parser();
+
+    // Get all the items in the RSS feed
+    const feed = await parser.parseURL("https://www.couponseagle.com/feed/"); // https://www.reddit.com/.rss
+
+    let items = [];
+
+    // Clean up the string and replace reserved characters
+    const fileName = `${feed.title.replace(/\s+/g, "-").replace(/[/\\?%*:|"<>]/g, '').toLowerCase()}.json`;
+
+    if (fs.existsSync(fileName)) {
+        items = require(`./${fileName}`);
+    }
+
+    // Add the items to the items array
+    await Promise.all(feed.items.map(async (currentItem) => {
+
+        // Add a new item if it doesn't already exist
+        if (items.filter((item) => isEquivalent(item, currentItem)).length <= 0) {
+            items.push(currentItem);
+        }
+
+    }));
+
+    // Save the file
+    fs.writeFileSync(fileName, JSON.stringify(items));
+
+})();
+
+function isEquivalent(a, b) {
+    // Create arrays of property names
+    let aProps = Object.getOwnPropertyNames(a);
+    let bProps = Object.getOwnPropertyNames(b);
+
+    // if number of properties is different, objects are not equivalent
+    if (aProps.length != bProps.length) {
+        return false;
+    }
+
+    for (let i = 0; i < aProps.length; i++) {
+        let propName = aProps[i];
+
+        // if values of same property are not equal, objects are not equivalent
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+
+    // if we made it this far, objects are considered equivalent
+    return true;
+}
+
 client.on ('ready', (c) => {
     console.log(`🤡 ${c.user.tag} is ready, nerds!`)
 })
 
 client.on('messageCreate', (message) => {
 
-    if(message.author.bot) {
-    return;
-    }
-    if (message.content === 'u cute') {
-        message.reply('no u')
-    return;
-    }
-    if(message.author.bot) {
-    return;
-    }
-    if (message.content === 'no u') {
-        message.reply('no, u')
-    return;
-    }
-    if (message.content === 'no u') {
-        message.reply('no, ur mum')
-    return;
-    }
-    if (message.content === 'hello') {
-        message.reply('heya nerd <3')
-    return;
-    }
-    if (message.content === 'hi') {
-        message.reply('yooo')
-    return;
-    }
-    if (message.content === 'hii') {
-        message.reply('yohoo')
-    return;
-    }
-    if (message.content === 'thanks') {
-        message.reply('no problem!')
-    return;
-    }
-    if (message.content === 'ty') {
-        message.reply('np!')
-    return;
-    }
-    if (message.content === 'tnx') {
-        message.reply('no problem!')
-    return;
-    }
-    if (message.content === '❤️') {
-        message.reply('❤️')
-    return;
-    }
-    if (message.content === 'jerry laugh my darling') {
-        message.reply('kekwkekwkekwkekwkekw :kekw: :kekw:')
-    return;
+    if (items.filter((item) => isEquivalent(item, currentItem)).length <= 0) {
+        message.send(currentItem);
     }
 });
 
